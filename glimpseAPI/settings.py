2f02d4338599bee0061a1ea442f2209c01c9e6d4
Ubuntu <ubuntu@ip-172-31-25-138.ec2.internal>
2018-10-17 04:28:43 +0000

    changed settings to run on the EC2 server

--- glimpseAPI/settings.py
+++ glimpseAPI/settings.py
@@ -23,10 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '@a+eplfdlt&es^ovkb+3^re+()02&i+@#_&gx+o_fdteod&7cj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['18.224.214.38',
-'127.0.0.1']
+ALLOWED_HOSTS = ['34.207.219.52']
 
 
 # Application definition
